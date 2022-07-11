a5f442f505812008fa9842b606674d43c940a2a7
Anand Jee Choubey <anandjeechoubey@Anands-MacBook-Air.local>
2022-07-12 01:11:34 +0530

    wip: amplify init

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "zingyreactnativeapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
